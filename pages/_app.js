c339574448872db4f02c6bf576f5852b5e3bd1cb
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-08 18:16:18 +0300

    Add ssr material ui support && disable FOUC

---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import App from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../src/theme'
+
+export default class MyApp extends App {
+  componentDidMount () {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>Gtfo cdo</title>
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    )
+  }
+}
