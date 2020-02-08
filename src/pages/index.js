b3c9deb3bb685efa24cffadd50f87282fc6bd59a
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-08 17:12:29 +0300

    Add standard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+function HomePage () {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default HomePage
