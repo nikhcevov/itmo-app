b5be697952f44e407dc1dd0da3a18725fed21b27
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-10 00:54:36 +0300

    Add monorepo root linter && some commands

--- server/index.js
+++ server/index.js
@@ -5,3 +5,16 @@ const port = 5000
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+async function print () {
+  const res = await fetch('https://de.ifmo.ru/?node=cde&index=persons', {
+    headers: {
+      'Content-Type': 'text/plain'
+    };
+  })
+  const ans = await res.text();
+
+  const el = document.createElement('html')
+  el.innerHTML = ans
+  console.log(el.getElementsByTagName('div'))
+}
