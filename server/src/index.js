564f29f7903fcf1c9c53e1d18129006f69a14294
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-10 03:08:25 +0300

    Add watchers schedule route to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,50 @@
 const express = require('express')
 const fetch = require('node-fetch')
-const jsdom = require('jsdom')
 
-const { JSDOM } = jsdom
 const app = express()
 const port = 5000
 
 app.get('/', (req, res) => res.send('Hello world'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-async function getWatchersRoutine () {
-  const res = await fetch('https://de.ifmo.ru/?node=cde&index=persons', {
-    headers: {
-      'Content-Type': 'text/plain'
+app.get('/schedule', async (req, res) => {
+  const q = await fetch('https://de.ifmo.ru/timesys/jsonp.xhtml')
+  const text = await q.text()
+  const persons = JSON.parse(text.slice(15).slice(0, -2))
+  const ans = []
+  for (const person of persons) {
+    const watcher = watchers.find(w => w.id === person.userId)
+    if (watcher !== undefined) {
+      ans.push({
+        name: watcher.name,
+        img: watcher.img,
+        schedule: person.schedule
+      })
     }
-  })
-  const { document } = (new JSDOM(await res.text())).window
-  console.log(document.getElementsByTagName('div'))
-}
+  }
+  res.send(ans)
+})
 
-getWatchersRoutine()
+app.listen(port, () => console.log(`Watchers app listening on port ${port}!`))
+
+const watchers = [
+  {
+    name: 'Кутейникова Марина Михайловна',
+    id: 730,
+    img: 'https://de.ifmo.ru/uploads/images/c685880c78738409.JPG'
+  },
+  {
+    name: 'Богданова Галина Николаевна',
+    id: 735,
+    img: 'https://de.ifmo.ru/uploads/images/7485880c76ed6c96.JPG'
+  },
+  {
+    name: 'Горбунова Татьяна Анатольевна',
+    id: 744,
+    img: 'https://de.ifmo.ru/uploads/images/5765880c7760d887.jpg'
+  },
+  {
+    name: 'Игнатьева Елена Юрьевна',
+    id: 729,
+    img: 'https://de.ifmo.ru/uploads/images/5275880c77c1974b.JPG'
+  }
+]
