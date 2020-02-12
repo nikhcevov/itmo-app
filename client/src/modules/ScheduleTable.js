1c5ea50a8a1d7d510a55f6fbdff7bd0edbf5425e
maxpavlov9991 <maxpavlov9991@gmail.com>
2020-02-13 00:29:05 +0300

    added modal window

--- client/src/modules/ScheduleTable.js
+++ client/src/modules/ScheduleTable.js
@@ -7,10 +7,17 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import SimpleDialog from './SimpleDialog'
 
 const useStyles = makeStyles({
   table: {
     minWidth: 320
+  },
+  row: {
+    '&:hover': {
+      backgroundColor: '#FFC0C0',
+      transition: 'background-color 0.1s'
+    }
   }
 })
 
@@ -40,28 +47,43 @@ function prepareData (data) {
 export default function SimpleTable ({ data, className }) {
   const classes = useStyles()
   const rows = prepareData(data)
+
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <TableContainer component={Paper} className={className}>
-      <Table className={classes.table} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Дата</TableCell>
-            <TableCell align='left'>Смотрящий</TableCell>
-            <TableCell align='left'>Сложность</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.date}>
-              <TableCell component='th' scope='row'>
-                {row.date}
-              </TableCell>
-              <TableCell align='left'>{row.name}</TableCell>
-              <TableCell align='left'>{row.difficulty}</TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Дата</TableCell>
+              <TableCell align='left'>Надзиратель</TableCell>
+              <TableCell align='left'>Сложность</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.date} onClick={handleClickOpen} className={classes.row}>
+                <TableCell component='th' scope='row'>
+                  {row.date}
+                </TableCell>
+                <TableCell align='left'>{row.name}</TableCell>
+                <TableCell align='left'>{row.difficulty}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <SimpleDialog open={open} onClose={handleClose} />
+    </div>
+
   )
 }
