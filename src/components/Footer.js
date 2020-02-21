71bffc4e48b93f53cce4819cc75685054bce4810
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-22 02:57:54 +0300

    Fix tabs flickering

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Grid from '@material-ui/core/Grid'
 
-import Link from '../components/Link'
+import Link from './Link'
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
