0986c6c33713983b119736e883215ed428a17dee
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-22 19:16:55 +0300

    Add light PWA support

--- next.config.js
+++ next.config.js
@@ -1,17 +1,34 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const withOffline = require('next-offline')
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        HOST_NAME: 'http://localhost:3000'
+const nextConfig = {
+  env: {
+    HOST_NAME: 'http://localhost:3000'
+  },
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
       }
-    }
-  }
-
-  return {
-    env: {
-      HOST_NAME: 'https://sb0101.ru'
-    }
+    ]
   }
 }
+
+module.exports = withOffline(nextConfig)
