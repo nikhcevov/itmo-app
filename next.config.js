13ba04cdb72d9b72c5715e1a6486d519d7a3869c
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-24 01:48:21 +0300

    Add fonts cache strategies

--- next.config.js
+++ next.config.js
@@ -6,22 +6,46 @@ const nextConfig = {
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: true,
+  // Docs: https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-webpack-plugin.GenerateSW#GenerateSW
   workboxOpts: {
     swDest: 'static/service-worker.js',
     runtimeCaching: [
+      // {
+      //   urlPattern: /^https?.*/,
+      //   handler: 'NetworkFirst',
+      //   options: {
+      //     cacheName: 'https-calls',
+      //     networkTimeoutSeconds: 15,
+      //     expiration: {
+      //       maxEntries: 150,
+      //       maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+      //     },
+      //     cacheableResponse: {
+      //       statuses: [0, 200]
+      //     }
+      //   }
+      // },
       {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
+        // Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+        urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+        handler: 'CacheFirst',
         options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
+          cacheName: 'google-fonts-webfonts',
           expiration: {
             maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
           },
           cacheableResponse: {
             statuses: [0, 200]
           }
+        }
+      },
+      {
+      // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+        urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'google-fonts-stylesheets'
         }
       }
     ]
