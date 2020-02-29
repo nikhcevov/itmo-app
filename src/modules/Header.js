5a5ec18217b0354dbb90a67b968281919ebe6392
Vladimir Ovechkin <ovchingus@gmail.com>
2020-02-29 03:59:56 +0300

    Add login page

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import MenuIcon from '@material-ui/icons/Menu'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import BrightnessDark from '@material-ui/icons/Brightness4'
@@ -45,7 +44,11 @@ export default function Header ({ handleMenuShow, currentPath, handleThemeChange
           >
             <BrightnessLight />
           </IconButton>
-          <Button color='inherit'>Login</Button>
+          <Button
+            color='inherit'
+            href='/login'
+          >Login
+          </Button>
         </div>
       </Toolbar>
     </AppBar>
