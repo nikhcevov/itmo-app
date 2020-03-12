c11f8fdc34a604cb67d7789315526fb45b1b2450
maxpavlov9991 <maxpavlov9991@gmail.com>
2020-03-12 04:48:08 +0300

    add group schedule search on schedule page

--- packages/itmo-app-client/src/modules/Schedule/Container.js
+++ packages/itmo-app-client/src/modules/Schedule/Container.js
@@ -1,26 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Card from './Card'
 
 const useStyles = makeStyles(theme => ({
   container: {
     '& > :not(:last-child)': {
       marginBottom: theme.spacing(2)
-    },
-    display: 'grid'
+    }
+  },
+  textField: {
+    marginBottom: theme.spacing(1)
+  },
+  switchButtonGroup: {
+    marginBottom: theme.spacing(1)
+  },
+  switchButton: {
   }
+
 }))
 
-export default function Container ({ data }) {
+export default function Container ({ data, group, setGroup }) {
   const classes = useStyles()
-  console.log(data)
+  const [isOdd, setWeekType] = useState(true)
+  const [isNotValid, setNotValid] = useState(false)
+
+  function handleChange (event) {
+    if (event.target.value.length < 8) {
+      if (isNotValid) setNotValid(false)
+      setGroup(event.target.value)
+    } else {
+      setNotValid(true)
+    }
+  }
+
+  function handleOddClick () {
+    setWeekType(true)
+  }
+
+  function handleEvenClick () {
+    setWeekType(false)
+  }
+
   return (
-    <div className={classes.container}>
-      {(data.odd) &&
-      data.odd.map((card) => (
-        <Card key={card.weekDay} data={card} />
-      ))}
-    </div>
+    <>
+      <TextField
+        id='standard-basic'
+        autoFocus
+        label='Группа'
+        value={group}
+        autoComplete='off'
+        spellCheck='false'
+        fullWidth
+        onChange={handleChange}
+        placeholder='Введите номер учебной группы'
+        variant='outlined'
+        color='secondary'
+        className={classes.textField}
+        error={isNotValid}
+        helperText={isNotValid && '7 symbols maximum!'}
+      />
+      <ButtonGroup color='secondary' className={classes.switchButtonGroup} fullWidth>
+        <Button variant={isOdd ? 'contained' : 'outlined'} onClick={handleOddClick}>Нечётная</Button>
+        <Button variant={isOdd ? 'outlined' : 'contained'} onClick={handleEvenClick}>Чётная</Button>
+      </ButtonGroup>
+      <div className={classes.container}>
+        {isOdd && data.odd &&
+            data.odd.map((card) => (
+              <Card key={card.weekDay} data={card} />
+            ))}
+
+        {!isOdd && data.even &&
+            data.even.map((card) => (
+              <Card key={card.weekDay} data={card} />
+            ))}
+      </div>
+    </>
   )
 }
--- packages/itmo-app-client/src/utils/postFetcher.js
+++
@@ -1,6 +0,0 @@
-// const HOST = process.env.HOST_NAME
-export default async function postFetcher (path, data) {
-  const res = await fetch(process.env.HOST_API + path, { method: 'POST', body: data })
-  const json = await res.json()
-  return json
-}
