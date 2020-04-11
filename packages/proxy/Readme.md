e8d4cf024a3bbfb682c0a7e73e83a4d53caca614
Vladimir Ovechkin <ovchingus@gmail.com>
2020-04-11 18:24:46 +0300

    --wip-- [skip ci]

---
+++ packages/proxy/Readme.md
@@ -0,0 +1,6 @@
+# Proxy server config
+
+### How to work with
+ * Set up variables in `.env` file
+ * Run `bootstrap.sh` to generate production configs in `deploy` folder
+ This is need to do only once 
--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+echo '1. Generate production proxy configs'
+cd ./packages/proxy
+chmod u+x ./bootstrap.sh
+./bootstrap.sh
+echo '1. Success'
+
+echo '2. Start docker compose'
+cd ../../
 docker-compose up --build
