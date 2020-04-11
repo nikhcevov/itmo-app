0bffb0d79caed0a799809555321c6f169c457f67
Vladimir Ovechkin <ovchingus@gmail.com>
2020-04-11 20:08:41 +0300

    --wip-- [skip ci]

--- packages/proxy/bootstrap.sh
+++ packages/proxy/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Export envs
+. ./.env
 REGEX=$(perl -e 'print "\$$_" for grep /^[_a-zA-Z]\w*$/, keys %ENV')
 
 echo '1. Recreate deploy dir'
@@ -8,18 +10,13 @@ mkdir ./deploy/nginx
 
 echo '2. Generate production files from templates'
 
-echo '2.1 Add environment variables'
-set -a
-source ./.env
-set +a
-
-echo '2.2 Generate server configuration for nginx'
+echo '2.1 Generate server configuration for nginx'
 envsubst "$REGEX" <./nginx/server.conf &>./deploy/nginx/server.conf
 
-echo '2.3 Generate client configuration for nginx'
+echo '2.2 Generate client configuration for nginx'
 envsubst "$REGEX" <./nginx/client.conf &>./deploy/nginx/client.conf
 
-echo '2.4 Generate certbot init script'
+echo '2.3 Generate certbot init script'
 envsubst "$REGEX" <./init-letsencrypt.sh &>./deploy/init-letsencrypt.sh
 
 echo '3. Start certbot init'
