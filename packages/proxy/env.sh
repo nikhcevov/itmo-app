48c87c8e99e82905eddf69d0d4b5733f265c3543
Vladimir Ovechkin <ovchingus@gmail.com>
2020-04-12 15:54:39 +0300

    --wip-- [skip ci]

--- packages/proxy/env.sh
+++ packages/proxy/env.sh
@@ -3,11 +3,11 @@ export PATH_CERTBOT_CHALLENGE_FILES=/var/www/certbot
 export PATH_CERTS_FILES=/etc/letsencrypt
 
 # Feel free to change
-export API_SERVER_DOMAIN=api.sb0101.online
+export API_SERVER_DOMAIN=api.sb0101.ru
 export API_SERVER_LOCAL_PORT=5000
 export API_SERVER_LOCAL_NAME=server
 export DOMAIN_MAIL=mail@sb0101.ru
 
-export CLIENT_DOMAIN=sb0101.online
+export CLIENT_DOMAIN=sb0101.ru
 export CLIENT_COMPILED_PATH=/itmo-app/client/build
 
